765e120f2687b81513b30bfd5413d9a7d13ef2ae
Charlie Pitkin <charlie.pitkin@gmail.com>
2019-09-16 14:01:28 +0200

    Added timestamp, Added openfaas deployment
    This project is ran with OpenFaaS

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # amzn-watchlist-price-check
+
+Checks prices of books on an Amazon public wishlist.
+
+## Why I built this tool
+
+### Problem
+
+You want to know when you favorite books are on sale to get the best deal. The problem is Amazon doesn't always tell you when a book goes on sale for one or two days leaving you to miss out on a great deal.
+
+### Solution
+
+If you put all your books in a public whish list we can parse the list and see the prices. When we do this two to four times a day you will be able to tell when books go on sale. This makes it easy to get the best deal on your favorite books.
+
+**NOTE:** All links and URLs use smile.amazon.com
+
+## Requirements
+
+1. The wishlist must be **PUBILC**
+
+## Tunables
+
+1. You can cover multiple wishlists and get a composite result back. We take the wishlist id and use that to fetch the list. See the example below on how to find the wish list id.
+  Amazon URL: `https://smile.amazon.com/hz/wishlist/ls/3GMM56QSK63GT`
+  Wishlist Id: `3GMM56QSK63GT`
+2. You can set the max price you want to pay for any given book. See [Price](#price) for details
+
+
+## Price
+
+The max price comparison uses the whole dollar amount to avoid extra conversion and rounding with cents.
+
+**Example:**
+Actual book price: $2.99
+Max Price value: 2
+
+The above example will return any book that is $2.99 or lower.
+
+## Output
+
+The out is an HTML formatted string meant to be placed in an email
+
+```html
+<b>Name:</b> Constitution: Book 1 of The Legacy Fleet Series
+<b>Price:</b> $3.99
+<a href="https://smile.amazon.com/dp/B010L6JTO0/?coliid=I17MWIZQ9RA5TN&colid=1DJLN9PNW8R59&psc=0">Buy Now</a>
+```
---
+++ handler.go
@@ -0,0 +1,113 @@
+// Copyright (c) 2019 Charlie Pitkin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package function
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+type Book struct {
+	Name  string
+	Price string
+	Link  string
+}
+
+// Collect all the books below a set price
+var allBooks = []Book{}
+
+// Price to the nearest whole number
+var maxPrice int64 = 2
+
+func parseList(e *colly.HTMLElement) {
+	e.ForEach("li.a-spacing-none", func(_ int, e *colly.HTMLElement) {
+
+		wholePrice := strings.Split(e.ChildText("span.a-price-whole"), ".")
+
+		numPrice, err := strconv.ParseInt(wholePrice[0], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		if numPrice <= maxPrice {
+			book := Book{
+				Name:  e.ChildAttr("a.a-link-normal", "title"),
+				Price: e.ChildText("span.a-price > span.a-offscreen"),
+				Link:  "https://smile.amazon.com" + e.ChildAttr("a.a-link-normal", "href"),
+			}
+
+			allBooks = append(allBooks, book)
+		}
+	})
+}
+
+func Handle(req []byte) string {
+	wishlistIdsString, err := ioutil.ReadFile("/var/openfaas/secrets/amzn_wishlist_ids")
+	if err != nil {
+		return err.Error()
+	}
+
+	zapierWebhook, err := ioutil.ReadFile("/var/openfaas/secrets/zapier_webhook")
+	if err != nil {
+		return err.Error()
+	}
+
+	wishlistIds := strings.Split(string(wishlistIdsString), ",")
+
+	c := colly.NewCollector(
+		colly.UserAgent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0"),
+	)
+
+	c.Limit(&colly.LimitRule{
+		DomainGlob:  "*.amazon.*",
+		Parallelism: 1,
+		Delay:       2 * time.Second,
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting: ", r.URL.String())
+	})
+
+	c.OnHTML("#g-items", func(e *colly.HTMLElement) {
+		parseList(e)
+		seeMoreLink := e.ChildAttr("a.wl-see-more", "href")
+		if seeMoreLink != "" {
+			c.Visit("https://smile.amazon.com" + seeMoreLink)
+		}
+	})
+
+	for _, id := range wishlistIds {
+		c.Visit("https://smile.amazon.com/hz/wishlist/ls/" + id)
+	}
+
+	emailString := ""
+
+	for _, book := range allBooks {
+		emailString = emailString + "<br><b>Name:</b> " + book.Name + "<br><b>Price:</b> " + book.Price + "<br><a href=\"" + book.Link + "\">Buy Now</a><br>"
+	}
+
+	timeString := time.Now()
+
+	fetchedTime := "Books fetched on " + string(timeString.Format(time.UnixDate)+"<br>")
+
+	jsonValues := map[string]string{"fetchedTime": fetchedTime, "books": emailString}
+
+	jsonValue, _ := json.Marshal(jsonValues)
+
+	res, err := http.Post(string(zapierWebhook), "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		return err.Error()
+	}
+	return string(res.Status)
+}
